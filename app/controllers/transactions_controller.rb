31a9e758b91c5d8a1abc029cf93afa255b65c68e
Jeremy <senn.jeremy@gmail.com>
2018-07-30 13:10:55 -0700

    Include link to customer on transaction show page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -50,6 +50,7 @@ class TransactionsController < ApplicationController
   def show
     @reversal_transaction = @transaction.reversal_transaction
     @original_transaction = @transaction.original_transaction
+    @from_customer = @transaction.from_account_customer
     @to_customer = @transaction.to_account_customer
   end
 
