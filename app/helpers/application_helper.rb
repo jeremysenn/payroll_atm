c12df99a4c221fa928f299cbb20d0723874519ef
Jeremy <senn.jeremy@gmail.com>
2018-12-27 14:00:14 -0800

    Don't try and truncate device state description if there isn't one found

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       "Wait Present", "Wait Totals", "Wait Reset Totals", "Wait Present", "Wait Test Dispense", "Wait Test Reject", "Wait Totals pre-Dispense", 
       "Wait Totals after Timeout", "Wait for Reset on bad dispense", "No Server", "Unknown", "Wait for ID", "Wait for Read Cassettes before redispense", 
       "Waiting for notes to be rejected", "Waiting for notes to be rejected", "Waiting for Lifts Up", "Waiting for new key", "Waiting for user response", 
-      "Waiting for Bill Pay screen", "Waiting for bill pay amount", "Waiting for Bill Pay Confirmation", "N/A"]
+      "Waiting for Bill Pay screen", "Waiting for bill pay amount", "Waiting for Bill Pay Confirmation", "N/A", "N/A", "N/A"]
   end
   
 end
